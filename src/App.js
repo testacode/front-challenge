c49d808acd5f40adc9fbffffd9f8ac52e68a209d
Carlos Monti <bastadespammers@gmail.com>
2022-08-17 16:18:27 -0300

    feat: adding routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import { Detail, Main } from './Pages';
+
+const URL = "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,DOGE,BNB,LTC,ADA,BUSD,BCH,VET,DOT,EOS,SOL,SRM,USDT,BTT,TRX,FIL,LINK,MATIC,UNI,NEO,CHZ,ETC,THETA,XLM,BSV,LUNA,WIN,SXP&tsyms=USD";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [cryptos, setCryptos] = useState({});
 
   useEffect(() => {
-    const url =
-      "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,DOGE,BNB,LTC,ADA,BUSD,BCH,VET,DOT,EOS,SOL,SRM,USDT,BTT,TRX,FIL,LINK,MATIC,UNI,NEO,CHZ,ETC,THETA,XLM,BSV,LUNA,WIN,SXP&tsyms=USD";
-
-    fetch(url)
+    fetch(URL)
       .then((res) => {
         if (res.ok) {
           return res.json();
@@ -43,40 +44,17 @@ const App = () => {
   });
 
   return (
-    <div className="App">
-      <div>
-        <label>Filter By</label>
-        <select>
-          <option>Name</option>
-          <option>Price</option>
-          <option>Market Cap</option>
-          <option>Circulating supply</option>
-        </select>
-      </div>
-      <table>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-          <th>Market Cap</th>
-          <th>Circulating supply</th>
-        </tr>
-
-        <tbody>
-          {formattedData.map((data) => {
-            return (
-              <tr>
-                <td>Name: {data.name}</td>
-                <td>Price: {data.price}</td>
-                <td>Market Cap: {data.market_cap}</td>
-                <td>Circulating supply: {data.circulatingSupply}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Main isLoading={isLoading} data={formattedData} />} />
+        <Route path="/detail/:coin" element={<Detail />} />
+        <Route
+          path="*"
+          element={<Navigate to="/" replace />}
+        />
+      </Routes>
+    </>
   );
-
 };
 
 export default App;
