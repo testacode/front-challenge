587b92a6f31e5d8454457520664bb0d38f3003fb
Carlos Monti <bastadespammers@gmail.com>
2022-08-17 15:27:57 -0300

    feat: moving from class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [cryptos, setCryptos] = useState({});
 
-    this.state = {
-      loading: false,
-      cryptos: {},
-    };
-  }
-
-  componentDidMount() {
-    this.setState({
-      loading: true,
-    });
+  useEffect(() => {
     const url =
       "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,DOGE,BNB,LTC,ADA,BUSD,BCH,VET,DOT,EOS,SOL,SRM,USDT,BTT,TRX,FIL,LINK,MATIC,UNI,NEO,CHZ,ETC,THETA,XLM,BSV,LUNA,WIN,SXP&tsyms=USD";
 
@@ -29,64 +20,63 @@ export default class App extends React.Component {
 
         data = json.RAW;
 
-        this.setState({
-          loading: false,
-          cryptos: data,
-        });
+        setCryptos(data);
       })
-      .catch((err) => console.log(err));
-  }
-
-  render() {
-    if (this.state.loading) {
-      return <div>loading...</div>;
-    }
-
-    let formattedData = [];
-
-    Object.keys(this.state.cryptos).map((crypto) => {
-      formattedData.push({
-        key: crypto,
-        price: this.state.cryptos[crypto]["USD"]["PRICE"],
-        market_cap: this.state.cryptos[crypto]["USD"].MKTCAP,
-        circulatingSupply: this.state.cryptos[crypto]["USD"].SUPPLY,
-        name: this.state.cryptos[crypto]["USD"]["FROMSYMBOL"],
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setIsLoading(false);
       });
+  }, []);
+
+  if (isLoading) return <div>loading...</div>;
+
+  let formattedData = [];
+
+  Object.keys(cryptos).map((crypto) => {
+    formattedData.push({
+      key: crypto,
+      price: cryptos[crypto]["USD"]["PRICE"],
+      market_cap: cryptos[crypto]["USD"].MKTCAP,
+      circulatingSupply: cryptos[crypto]["USD"].SUPPLY,
+      name: cryptos[crypto]["USD"]["FROMSYMBOL"],
     });
+  });
 
-    return (
-      <div className="App">
-        <div>
-          <label>Filter By</label>
-          <select>
-            <option>Name</option>
-            <option>Price</option>
-            <option>Market Cap</option>
-            <option>Circulating supply</option>
-          </select>
-        </div>
-        <table>
-          <tr>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Market Cap</th>
-            <th>Circulating supply</th>
-          </tr>
-
-          <tbody>
-            {formattedData.map((data) => {
-              return (
-                <tr>
-                  <td>Name: {data.name}</td>
-                  <td>Price: {data.price}</td>
-                  <td>Market Cap: {data.market_cap}</td>
-                  <td>Circulating supply: {data.circulatingSupply}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+  return (
+    <div className="App">
+      <div>
+        <label>Filter By</label>
+        <select>
+          <option>Name</option>
+          <option>Price</option>
+          <option>Market Cap</option>
+          <option>Circulating supply</option>
+        </select>
       </div>
-    );
-  }
-}
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+          <th>Market Cap</th>
+          <th>Circulating supply</th>
+        </tr>
+
+        <tbody>
+          {formattedData.map((data) => {
+            return (
+              <tr>
+                <td>Name: {data.name}</td>
+                <td>Price: {data.price}</td>
+                <td>Market Cap: {data.market_cap}</td>
+                <td>Circulating supply: {data.circulatingSupply}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+
+};
+
+export default App;
